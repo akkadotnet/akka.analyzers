c12bd30bfea10fb8516d0e1735a2a0f889f84b48
Aaron Stannard <aaron@petabridge.com>
2023-12-27 23:02:34 -0600

    AK1001: must close over `Sender` when using `PipeTo` (#5)
    * AK1001: must close over `Sender` when using `PipeTo`
    
    Not 100% sure that this is an issue in newer versions of Akka.NET, but it's still a good idea to enforce this.
    
    * added success cases
    
    * added failure case
    
    * fixed analyzer being referenced
    
    * riding the strugglebus
    
    * have first unhappy path passing
    
    * parameterized span counts for failure cases

---
+++ src/Akka.Analyzers.Tests/Analyzers/AK1000/MustCloseOverSenderWhenUsingPipeToSpecs.cs
@@ -0,0 +1,124 @@
+using Microsoft.CodeAnalysis;
+using Verify = Akka.Analyzers.Tests.Utility.AkkaVerifier<Akka.Analyzers.MustCloseOverSenderWhenUsingPipeToAnalyzer>;
+
+namespace Akka.Analyzers.Tests.Analyzers.AK1000;
+
+public class MustCloseOverSenderWhenUsingPipeToSpecs
+{
+    public static readonly TheoryData<string> SuccessCases = new()
+    {
+        // ReceiveActor using PipeTo with a closure inside a Receive<string> block
+        @"using Akka.Actor;
+        using System.Threading.Tasks;
+
+        public sealed class MyActor : ReceiveActor{
+
+        public MyActor(){
+            Receive<string>(str => {
+             async Task<int> LocalFunction(){
+                  await Task.Delay(10);
+                  return str.Length;
+             }
+
+             // correct use of closure
+             var sender = Sender;
+             LocalFunction().PipeTo(sender); 
+         });
+        }
+      }",
+
+        // ReceiveActor using PipeTo calling a method from within a Receive<T> block
+        @"using Akka.Actor;
+        using System.Threading.Tasks;
+
+        public sealed class MyActor : ReceiveActor{
+
+        public MyActor(){
+            Receive<string>(str => {
+                Execute(str);
+            });
+        }
+
+        private void Execute(string str){
+           async Task<int> LocalFunction(){
+                  await Task.Delay(10);
+                  return str.Length;
+             }
+
+             // correct use of closure
+             var sender = Sender;
+             LocalFunction().PipeTo(sender); 
+        }
+    };",
+
+        // Actor doing message handling in a Receive<T> block without PipeTo at all
+        @"using Akka.Actor;
+        
+        public sealed class MyActor : ReceiveActor{
+
+            public MyActor(){
+                Receive<string>(str => {
+                    Sender.Tell(str); // shouldn't flag this
+                });
+            }
+        }"
+    };
+    
+    [Theory]
+    [MemberData(nameof(SuccessCases))]
+    public async Task SuccessCase(string testCode)
+    {
+        await Verify.VerifyAnalyzer(testCode).ConfigureAwait(true);
+    }
+
+    public static readonly TheoryData<(string testData, (int startLine, int startColumn, int endLine, int endColumn) spanData)> FailureCases = new()
+    {
+        // Receive actor using PipeTo without a closure inside a Receive<T> block
+        (@"using Akka.Actor;
+        using System.Threading.Tasks;
+        
+        public sealed class MyActor : ReceiveActor{
+
+            public MyActor(){
+                Receive<string>(str => {
+                    async Task<int> LocalFunction(){
+                        await Task.Delay(10);
+                        return str.Length;
+                    }
+
+                    // incorrect use of closure
+                    LocalFunction().PipeTo(Sender); 
+                });
+            }
+        }", (14, 37, 14, 43)),
+        
+        // UntypedActor using PipeTo without a closure inside a OnReceive block
+        (@"using Akka.Actor;
+        using System.Threading.Tasks;
+
+        public sealed class MyActor : UntypedActor{
+
+            protected override void OnReceive(object message){
+                async Task<int> LocalFunction(){
+                    await Task.Delay(10);
+                    return message.ToString().Length;
+                }
+
+                // incorrect use of closure
+                LocalFunction().PipeTo(Sender); 
+            }
+        }", (13, 33, 13, 39)),
+    };
+    
+    [Theory]
+    [MemberData(nameof(FailureCases))]
+    public async Task FailureCase((string testCode, (int startLine, int startColumn, int endLine, int endColumn) spanData) d)
+    {
+        var expected = Verify.Diagnostic()
+            .WithSpan(d.spanData.startLine, d.spanData.startColumn, d.spanData.endLine, d.spanData.endColumn)
+            .WithArguments("Sender")
+            .WithSeverity(DiagnosticSeverity.Error);
+        
+        await Verify.VerifyAnalyzer(d.testCode, expected).ConfigureAwait(true);
+    }
+}
