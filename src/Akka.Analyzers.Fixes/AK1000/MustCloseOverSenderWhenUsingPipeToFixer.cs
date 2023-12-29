5482a513ecbbc54d87fb031d15ebdbd70140c0e9
Aaron Stannard <aaron@petabridge.com>
2023-12-29 11:40:39 -0600

    implemented first pass at a `PipeTo(Sender)` CodeFixer (#15)
    * implemented first pass at a `PipeTo(Sender)` CodeFixer
    
    * renaming existing Analyzer specs
    
    so there won't be naming conflicts or confusion with Fixer specs that target the same ruleset
    
    * refactored `Verify` to include possibility of CodeFix tests
    
    * rename analyzer test files
    
    * trying to debug
    
    * restore proper test behavior
    
    * fixed spec
    
    Roslyn is still not detecting the fixer correctly though
    
    * working on fixing "Fixer" discovery
    
    * removed duplicate project reference
    
    * fixed discovery
    
    * have fixer working, now fighting over whitespace
    
    * fixed whitespace mangling
    
    * removed offending lines from formatter
    
    * verified FixIt cases for PipeTo

---
+++ src/Akka.Analyzers.Fixes/AK1000/MustCloseOverSenderWhenUsingPipeToFixer.cs
@@ -0,0 +1,84 @@
+using System.Composition;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Editing;
+using Microsoft.CodeAnalysis.Formatting;
+using static Akka.Analyzers.Fixes.CodeGeneratorUtilities;
+
+namespace Akka.Analyzers.Fixes.AK1000;
+
+[ExportCodeFixProvider(LanguageNames.CSharp), Shared]
+public sealed class MustCloseOverSenderWhenUsingPipeToFixer()
+    : BatchedCodeFixProvider(RuleDescriptors.Ak1001CloseOverSenderUsingPipeTo.Id)
+{
+    public const string Key_FixPipeToSender = "AK1000_FixPipeToSender";
+
+    public override async Task RegisterCodeFixesAsync(CodeFixContext context)
+    {
+        var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+        if (root is null)
+            return;
+
+        var diagnostic = context.Diagnostics.FirstOrDefault();
+        if (diagnostic is null)
+            return;
+        var diagnosticSpan = diagnostic.Location.SourceSpan;
+
+        // Find the PipeTo invocation expression
+        var invocationExpr = root.FindToken(diagnosticSpan.Start).Parent?.AncestorsAndSelf()
+            .OfType<InvocationExpressionSyntax>().First();
+        if (invocationExpr is null)
+            return;
+
+        context.RegisterCodeFix(
+            CodeAction.Create(
+                title: "Use local variable for Sender",
+                createChangedDocument: c => UseLocalVariableForSenderAsync(context.Document, invocationExpr, c),
+                equivalenceKey: Key_FixPipeToSender),
+            context.Diagnostics);
+    }
+
+    private static async Task<Document> UseLocalVariableForSenderAsync(Document document,
+        InvocationExpressionSyntax invocationExpr, CancellationToken cancellationToken)
+    {
+        var editor = await DocumentEditor.CreateAsync(document, cancellationToken).ConfigureAwait(false);
+
+        // Generate a local variable to hold 'Sender'
+        var senderVariable = IntroduceLocalVariableStatement("sender", SyntaxFactory.MemberAccessExpression(
+            SyntaxKind.SimpleMemberAccessExpression,
+            SyntaxFactory.ThisExpression(),
+            SyntaxFactory.IdentifierName("Sender")))
+                .WithAdditionalAnnotations(Formatter.Annotation); // need this line to get indentation right
+        
+        // Find an appropriate insertion point for the local variable
+        var insertionPoint = invocationExpr.FirstAncestorOrSelf<StatementSyntax>();
+
+        if (insertionPoint == null)
+        {
+            // Unable to find a valid insertion point
+            return document;
+        }
+        
+        // Insert the local variable declaration at the found insertion point
+        editor.InsertBefore(insertionPoint, senderVariable);
+        
+        // in the invocationExpr, replace the old argument list with a new one that uses the new local variable
+        // Replace the invocation arguments with the new local variable
+        var newArgumentList = SyntaxFactory.ArgumentList(
+            SyntaxFactory.SingletonSeparatedList(
+                SyntaxFactory.Argument(
+                    SyntaxFactory.IdentifierName("sender"))));
+
+        var newInvocationExpr = invocationExpr.WithArgumentList(newArgumentList);
+
+        // Make sure to replace the old invocation with the new one
+        editor.ReplaceNode(invocationExpr, newInvocationExpr);
+        
+        var newDocument = editor.GetChangedDocument(); // error happens here
+
+        return newDocument;
+    }
+}
---
+++ src/Akka.Analyzers.Tests/Fixes/AK1000/MustCloseOverSenderWhenUsingPipeToFixerSpecs.cs
@@ -0,0 +1,107 @@
+using Akka.Analyzers.Fixes.AK1000;
+using Verify = Akka.Analyzers.Tests.Utility.AkkaVerifier<Akka.Analyzers.MustCloseOverSenderWhenUsingPipeToAnalyzer>;
+
+namespace Akka.Analyzers.Tests.Fixes.AK1000;
+
+public class MustCloseOverSenderWhenUsingPipeToFixerSpecs
+{
+    [Fact]
+    public Task AddClosureInsideReceiveActor()
+    {
+        var before =
+            @"using Akka.Actor;
+using System.Threading.Tasks;
+
+public sealed class MyActor : ReceiveActor{
+
+    public MyActor(){
+        Receive<string>(str => {
+            async Task<int> LocalFunction(){
+                await Task.Delay(10);
+                return str.Length;
+            }
+
+            // incorrect use of closure
+            LocalFunction().PipeTo(Sender); 
+        });
+    }
+}";
+
+        var after =
+            @"using Akka.Actor;
+using System.Threading.Tasks;
+
+public sealed class MyActor : ReceiveActor{
+
+    public MyActor(){
+        Receive<string>(str => {
+            async Task<int> LocalFunction(){
+                await Task.Delay(10);
+                return str.Length;
+            }
+            var sender = this.Sender;
+
+            // incorrect use of closure
+            LocalFunction().PipeTo(sender); 
+        });
+    }
+}";
+
+        var expectedDiagnostic = Verify.Diagnostic()
+            .WithSpan(14, 29, 14, 35)
+            .WithArguments("Sender");
+
+        return Verify.VerifyCodeFix(before, after, MustCloseOverSenderWhenUsingPipeToFixer.Key_FixPipeToSender,
+            expectedDiagnostic);
+    }
+
+    [Fact]
+    public Task AddClosureInsideUntypedActor()
+    {
+        var before = 
+@"using Akka.Actor;
+using System.Threading.Tasks;
+using System;
+
+public sealed class MyActor : UntypedActor{
+
+    protected override void OnReceive(object message){
+        async Task<int> LocalFunction(){
+            await Task.Delay(10);
+            return message.ToString().Length;
+        }
+
+        Console.WriteLine(Sender);
+        // incorrect use of closure
+        LocalFunction().PipeTo(Sender); 
+    }
+}";
+        
+        var after = 
+@"using Akka.Actor;
+using System.Threading.Tasks;
+using System;
+
+public sealed class MyActor : UntypedActor{
+
+    protected override void OnReceive(object message){
+        async Task<int> LocalFunction(){
+            await Task.Delay(10);
+            return message.ToString().Length;
+        }
+
+        Console.WriteLine(Sender);
+        var sender = this.Sender;
+        // incorrect use of closure
+        LocalFunction().PipeTo(sender); 
+    }
+}";
+        
+        var expectedDiagnostic = Verify.Diagnostic()
+            .WithSpan(15, 25, 15, 31)
+            .WithArguments("Sender");
+
+        return Verify.VerifyCodeFix(before, after, MustCloseOverSenderWhenUsingPipeToFixer.Key_FixPipeToSender,
+            expectedDiagnostic);
+    }
+}
