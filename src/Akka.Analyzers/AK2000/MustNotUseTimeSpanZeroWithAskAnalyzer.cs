57f1948d3269a08043f78c3f85af963e7c272d92
Aaron Stannard <aaron@petabridge.com>
2023-12-29 14:26:25 -0600

    have tests finally starting to pass

--- src/Akka.Analyzers/AK2000/MustNotUseTimeSpanZeroWithAskAnalyzer.cs
+++ src/Akka.Analyzers/AK2000/MustNotUseTimeSpanZeroWithAskAnalyzer.cs
@@ -9,10 +9,11 @@ using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 
-namespace Akka.Analyzers;
+namespace Akka.Analyzers.AK2000;
 
 [DiagnosticAnalyzer(LanguageNames.CSharp)]
-public class MustNotUseTimeSpanZeroWithAskAnalyzer() : AkkaDiagnosticAnalyzer(RuleDescriptors.Ak2000DoNotUseZeroTimeoutWithAsk)
+public class MustNotUseTimeSpanZeroWithAskAnalyzer()
+    : AkkaDiagnosticAnalyzer(RuleDescriptors.Ak2000DoNotUseZeroTimeoutWithAsk)
 {
     public override void AnalyzeCompilation(CompilationStartAnalysisContext context, AkkaContext akkaContext)
     {
@@ -21,27 +22,35 @@ public class MustNotUseTimeSpanZeroWithAskAnalyzer() : AkkaDiagnosticAnalyzer(Ru
 
         context.RegisterSyntaxNodeAction(ctx =>
         {
-            var invocation = (InvocationExpressionSyntax)ctx.Node;
+            var invocationExpr = (InvocationExpressionSyntax)ctx.Node;
 
-            if (ModelExtensions.GetSymbolInfo(ctx.SemanticModel, invocation).Symbol is not IMethodSymbol methodSymbol)
-                return;
-
-            // Check if it's a call to Ask
-            if (!IsMethodAsk(methodSymbol, akkaContext))
-                return;
-            
-            // Iterate over the arguments - check only the timespan ones
-            foreach (var arg in invocation.ArgumentList.Arguments)
+            if (ctx.SemanticModel.GetSymbolInfo(invocationExpr).Symbol is IMethodSymbol { Name: "Ask" } methodSymbol &&
+                methodSymbol.Parameters.Any(p => p.Type.ToString() == "System.TimeSpan?"))
             {
-                if (IsTimeSpanType(ctx.SemanticModel.GetTypeInfo(arg.Expression).ConvertedType))
+                foreach (var argument in invocationExpr.ArgumentList.Arguments)
                 {
-                    // Check if the TimeSpan argument is default or TimeSpan.Zero
-                    // Check if the argument is a problematic TimeSpan
-                    if (IsProblematicTimeSpan(arg.Expression, ctx))
+                    if (ctx.SemanticModel.GetTypeInfo(argument.Expression).Type is INamedTypeSymbol argType &&
+                        argType.ConstructedFrom.ToString() == "System.TimeSpan")
                     {
-                        var diagnostic = Diagnostic.Create(RuleDescriptors.Ak2000DoNotUseZeroTimeoutWithAsk, arg.GetLocation());
-                        ctx.ReportDiagnostic(diagnostic);
-                        break; // Report only once per invocation
+                        if (argument.Expression is MemberAccessExpressionSyntax { Name.Identifier.ValueText: "Zero" })
+                        {
+                            var diagnostic = Diagnostic.Create(RuleDescriptors.Ak2000DoNotUseZeroTimeoutWithAsk,
+                                argument.GetLocation());
+                            ctx.ReportDiagnostic(diagnostic);
+                        }
+                        else if (argument.Expression is IdentifierNameSyntax identifierName)
+                        {
+                            var symbol = ctx.SemanticModel.GetSymbolInfo(identifierName).Symbol;
+                            if (symbol is ILocalSymbol
+                                {
+                                    HasConstantValue: true, ConstantValue: not null
+                                } localSymbol && localSymbol.ConstantValue.Equals(TimeSpan.Zero))
+                            {
+                                var diagnostic = Diagnostic.Create(RuleDescriptors.Ak2000DoNotUseZeroTimeoutWithAsk,
+                                    argument.GetLocation());
+                                ctx.ReportDiagnostic(diagnostic);
+                            }
+                        }
                     }
                 }
             }
@@ -56,20 +65,15 @@ public class MustNotUseTimeSpanZeroWithAskAnalyzer() : AkkaDiagnosticAnalyzer(Ru
         // Check if the method is Ask or Ask<T>
         return methodSymbol.Name == "Ask" && methodSymbol.ContainingType.ToString().Contains("Akka.Actor");
     }
-    
-    private static bool IsTimeSpanType(ITypeSymbol? typeSymbol)
-    {
-        return typeSymbol != null && typeSymbol.ToString() == "System.TimeSpan";
-    }
-    
+
     private static bool IsProblematicTimeSpan(ExpressionSyntax? expression, SyntaxNodeAnalysisContext context)
     {
-        if(expression == null)
+        if (expression == null)
             return false;
-        
+
         // Handle cases where the argument is a literal or default value
         var expressionStr = expression.ToString();
-        if (expressionStr is "default" or "System.TimeSpan.Zero")
+        if (expressionStr is "default(TimeSpan)" or "System.TimeSpan.Zero")
         {
             return true;
         }
