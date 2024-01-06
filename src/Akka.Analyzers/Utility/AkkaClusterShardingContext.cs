0ab8bb9fee3413d1d9235291a246b14af655ceda
Aaron Stannard <aaron@petabridge.com>
2024-01-06 09:45:07 -0600

    working on sharding context

---
+++ src/Akka.Analyzers/Utility/AkkaClusterShardingContext.cs
@@ -0,0 +1,12 @@
+﻿// -----------------------------------------------------------------------
+//  &lt;copyright file="AkkaClusterShardingContext.cs" company="Akka.NET Project"&gt;&#xD;
+//      Copyright (C) 2013-2024 .NET Foundation &lt;https://github.com/akkadotnet/akka.net&gt;&#xD;
+//  &lt;/copyright&gt;&#xD;
+// -----------------------------------------------------------------------
+
+namespace Akka.Analyzers;
+
+public class AkkaClusterShardingContext
+{
+    
+}
