38a90f37369d6010b4af3b67baca365ca568aeb5
Aaron Stannard <aaron@petabridge.com>
2024-01-08 16:33:34 -0600

    creating some failure test cases

--- src/Akka.Analyzers.Tests/Fixes/AK2000/MustNotHandleAutomaticallyHandledMessagesInMessageExtractorFixerSpecs.cs
+++ src/Akka.Analyzers.Tests/Fixes/AK2000/MustNotHandleAutomaticallyHandledMessagesInMessageExtractorFixerSpecs.cs
@@ -73,4 +73,128 @@ public sealed class ShardMessageExtractor : HashCodeMessageExtractor
         return Verify.VerifyCodeFix(before, after, MustNotUseAutomaticallyHandledMessagesInsideMessageExtractorFixer.Key_FixAutomaticallyHandledShardedMessage,
             expectedDiagnostic);
     }
+    
+    [Fact]
+    public Task RemoveElseIfStatementFromMessageExtractor()
+    {
+        var before =
+            """
+            using Akka.Cluster.Sharding;
+            public sealed class ShardMessageExtractor : HashCodeMessageExtractor
+            {
+                /// <summary>
+                /// We only ever run with a maximum of two nodes, so ~10 shards per node
+                /// </summary>
+                public ShardMessageExtractor(int shardCount = 20) : base(shardCount)
+                {
+                }
+            
+                public override string EntityId(object message)
+                {
+                    if(message is string sharded)
+                    {
+                        return sharded;
+                    }
+                    else if (message is ShardingEnvelope e)
+                    {
+                        return e.EntityId;
+                    }
+                    
+                    return null;
+                }
+            }
+            """;
+
+        var after =
+            """
+            using Akka.Cluster.Sharding;
+            public sealed class ShardMessageExtractor : HashCodeMessageExtractor
+            {
+                /// <summary>
+                /// We only ever run with a maximum of two nodes, so ~10 shards per node
+                /// </summary>
+                public ShardMessageExtractor(int shardCount = 20) : base(shardCount)
+                {
+                }
+            
+                public override string EntityId(object message)
+                {
+                    if(message is string sharded)
+                    {
+                        return sharded;
+                    }
+            
+                    return null;
+                }
+            }
+            """;
+            
+        var expectedDiagnostic = Verify.Diagnostic()
+            .WithSpan(17, 29, 17, 47);
+
+        return Verify.VerifyCodeFix(before, after, MustNotUseAutomaticallyHandledMessagesInsideMessageExtractorFixer.Key_FixAutomaticallyHandledShardedMessage,
+            expectedDiagnostic);
+    }
+
+    [Fact]
+    public Task RemoveCaseStatementFromMessageExtractorWithSwitch()
+    {
+        var before =
+"""
+using Akka.Cluster.Sharding;
+public sealed class ShardMessageExtractor : HashCodeMessageExtractor
+{
+    /// <summary>
+    /// We only ever run with a maximum of two nodes, so ~10 shards per node
+    /// </summary>
+    public ShardMessageExtractor(int shardCount = 20) : base(shardCount)
+    {
+    }
+
+    public override string EntityId(object message)
+    {
+        switch(message)
+        {
+            case string sharded:
+                return sharded;
+            case ShardingEnvelope e:
+                return e.EntityId;
+            default:
+                return null;
+        }
+    }
+}
+""";
+
+        var after =
+"""
+using Akka.Cluster.Sharding;
+public sealed class ShardMessageExtractor : HashCodeMessageExtractor
+{
+    /// <summary>
+    /// We only ever run with a maximum of two nodes, so ~10 shards per node
+    /// </summary>
+    public ShardMessageExtractor(int shardCount = 20) : base(shardCount)
+    {
+    }
+
+    public override string EntityId(object message)
+    {
+        switch(message)
+        {
+            case string sharded:
+                return sharded;
+            default:
+                return null;
+        }
+    }
+}
+""";
+            
+        var expectedDiagnostic = Verify.Diagnostic()
+            .WithSpan(17, 18, 17, 36);
+
+        return Verify.VerifyCodeFix(before, after, MustNotUseAutomaticallyHandledMessagesInsideMessageExtractorFixer.Key_FixAutomaticallyHandledShardedMessage,
+            expectedDiagnostic);
+    }
 }
