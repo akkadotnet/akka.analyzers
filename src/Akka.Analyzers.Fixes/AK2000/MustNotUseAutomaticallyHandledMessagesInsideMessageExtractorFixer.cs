296e61c8720084cf955f84f40f6d00d8d7dc976a
Aaron Stannard <aaron@petabridge.com>
2024-01-08 19:43:03 -0600

    fixes

--- src/Akka.Analyzers.Fixes/AK2000/MustNotUseAutomaticallyHandledMessagesInsideMessageExtractorFixer.cs
+++ src/Akka.Analyzers.Fixes/AK2000/MustNotUseAutomaticallyHandledMessagesInsideMessageExtractorFixer.cs
@@ -8,6 +8,7 @@ using System.Composition;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.VisualBasic.Syntax;
 using IfStatementSyntax = Microsoft.CodeAnalysis.CSharp.Syntax.IfStatementSyntax;
