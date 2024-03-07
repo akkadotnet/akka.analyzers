364172bc6273a575920af413f3b3baf44bb524a0
Gregorius Soedharmo <arkatufus@yahoo.com>
2024-03-08 05:22:32 +0700

    User type symbol equality everywhere (#82)
    * Refactor AkkaContext for modelling complex contexts in the future
    
    * Fix naming consistency
    
    * Move AkkaContext from Utility to Context
    
    * Convert all codes to use strongly typed equality
    
    * Make sure that ReceiveAsync detection is as robust as possible
    
    * Fix Rider refactor artifact

---
+++ src/Akka.Analyzers/Context/Core/Actor/ActorContextContext.cs
@@ -0,0 +1,38 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="ActorContextContext.cs" company="Akka.NET Project">
+//      Copyright (C) 2013-2024 .NET Foundation <https://github.com/akkadotnet/akka.net>
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using Akka.Analyzers.Context.Core.Actor;
+using Microsoft.CodeAnalysis;
+
+namespace Akka.Analyzers.Core.Actor;
+
+public interface IActorContextContext
+{
+    public IPropertySymbol? Self { get; }
+}
+
+public sealed class EmptyActorContextContext : IActorContextContext
+{
+    public static readonly EmptyActorContextContext Instance = new();
+    private EmptyActorContextContext() { }
+    
+    public IPropertySymbol? Self => null;
+}
+
+public sealed class ActorContextContext : IActorContextContext
+{
+    private readonly Lazy<IPropertySymbol> _lazySelf;
+
+    private ActorContextContext(AkkaCoreActorContext context)
+    {
+        _lazySelf = new Lazy<IPropertySymbol>(() => (IPropertySymbol) context.IActorContextType!.GetMembers("Self").First());
+    }
+
+    public IPropertySymbol? Self => _lazySelf.Value;
+
+    public static ActorContextContext Get(AkkaCoreActorContext context)
+        => new(context);
+}
---
+++ src/Akka.Analyzers/Context/Core/Actor/ReceiveActorContext.cs
@@ -0,0 +1,46 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="ReceiveActorContext.cs" company="Akka.NET Project">
+//      Copyright (C) 2013-2024 .NET Foundation <https://github.com/akkadotnet/akka.net>
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+
+namespace Akka.Analyzers.Context.Core.Actor;
+
+public interface IReceiveActorContext
+{
+    public ImmutableArray<IMethodSymbol> ReceiveAsync { get; }
+    public ImmutableArray<IMethodSymbol> ReceiveAnyAsync { get; }
+}
+
+public sealed class EmptyReceiveActorContext : IReceiveActorContext
+{
+    public static readonly EmptyReceiveActorContext Instance = new();
+    
+    private EmptyReceiveActorContext() { }
+    
+    public ImmutableArray<IMethodSymbol> ReceiveAsync => new();
+    public ImmutableArray<IMethodSymbol> ReceiveAnyAsync => new();
+}
+
+public sealed class ReceiveActorContext : IReceiveActorContext
+{
+    private readonly Lazy<ImmutableArray<IMethodSymbol>> _lazyReceiveAsync;
+    private readonly Lazy<ImmutableArray<IMethodSymbol>> _lazyReceiveAnyAsync;
+
+    private ReceiveActorContext(AkkaCoreActorContext context)
+    {
+        _lazyReceiveAsync = new Lazy<ImmutableArray<IMethodSymbol>>(() => context.ReceiveActorType!
+            .GetMembers("ReceiveAsync").Select(m => (IMethodSymbol)m).ToImmutableArray());
+        _lazyReceiveAnyAsync = new Lazy<ImmutableArray<IMethodSymbol>>(() => context.ReceiveActorType!
+            .GetMembers("ReceiveAnyAsync").Select(m => (IMethodSymbol)m).ToImmutableArray());
+    }
+
+    public ImmutableArray<IMethodSymbol> ReceiveAsync => _lazyReceiveAsync.Value;
+    public ImmutableArray<IMethodSymbol> ReceiveAnyAsync => _lazyReceiveAnyAsync.Value;
+
+    public static ReceiveActorContext Get(AkkaCoreActorContext context)
+        => new(context);
+}
