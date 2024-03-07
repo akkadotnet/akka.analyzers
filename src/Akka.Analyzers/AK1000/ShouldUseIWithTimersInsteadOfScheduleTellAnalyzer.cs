7e3638d69a42ad7668802ca2868dba617cfd47b7
Gregorius Soedharmo <arkatufus@yahoo.com>
2024-03-07 22:42:15 +0700

    Fix merge problems

--- src/Akka.Analyzers/AK1000/ShouldUseIWithTimersInsteadOfScheduleTellAnalyzer.cs
+++ src/Akka.Analyzers/AK1000/ShouldUseIWithTimersInsteadOfScheduleTellAnalyzer.cs
@@ -5,6 +5,7 @@
 // -----------------------------------------------------------------------
 
 using System.Collections.Immutable;
+using Akka.Analyzers.Context;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
